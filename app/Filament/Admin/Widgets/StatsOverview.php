547e7e3cc21a86e6c9e5a043f551a3d27f815c76
Abdullah Barakat <abdallah771363885@gmail.com>
2024-02-18 20:54:07 +0300

    add some widgets

---
+++ app/Filament/Admin/Widgets/StatsOverview.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+        Stat::make('Bounce rate', '21%')
+            ->description('7% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-down'),
+        Stat::make('Average time on page', '3:12')
+            ->description('3% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up'),
+        Stat::make('Unique views', '300.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
+            ->color('success'),
+            Stat::make('Unique views', '192.1k')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
+            ->color('success'),
+        ];
+    }
+}
